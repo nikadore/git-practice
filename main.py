bbae0181bedf8e21fa014cb2e6cfedfeeb94c3c2
unknown <davitmetreveli2012@gmail.com>
2025-10-07 17:45:04 +0400

    ragaca

---
+++ main.py
@@ -0,0 +1,20 @@
+def luwkentoba(num):
+      if num % 2 == 0:
+            print ('true')
+      else:
+            print ('false')
+
+
+def jami(lst):
+      sum = 0
+      for i in lst:
+            sum += i
+      print(sum)
+
+
+
+luwkentoba(5)
+jami([5, 6, 7, 1])
+
+
+
